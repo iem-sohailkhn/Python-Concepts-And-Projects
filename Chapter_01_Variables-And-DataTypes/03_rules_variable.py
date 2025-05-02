9b1ec0aca25f08bf8506f1f619dc93eac73d1cbd
iem-sohailkhn <msohailabdullah22@gmail.com>
2025-05-02 15:45:47 +0500

    Delete Chapter_01_Variables-And-DataTypes directory

--- Chapter_01_Variables-And-DataTypes/03_rules_variable.py
+++
@@ -1,7 +0,0 @@
-a = 1
-aaa = 33
-Sohii = 333
-Sohail = 32
-_sufii = 55
-# @Sui = 32 invalid due to @ symbol
-# soh@il = 23 invalid due to @ symbol
--- Chapter_01_Variables-And-DataTypes/05_type.py
+++
@@ -1,7 +0,0 @@
-a = "31.2"
-b = float(a) # class <int> conversion of string to float
-str(31) ="31" # integer to string conversion
-int("32") = 32 # string to integer conversion
-float(32) = 32.0 # integer to float 
-t = type(b)
-print (t)
